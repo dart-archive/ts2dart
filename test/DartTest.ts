f5605f45e05a8b0bdc405703e2f1539c6758fe14
Martin Probst <martin@probst.io>
2015-03-08 21:20:08 -0700

    Method/function parameters. Some tests for functions.

--- test/DartTest.ts
+++ test/DartTest.ts
@@ -1,8 +1,8 @@
-/// <reference path="../typings/chai/chai.d.ts"/>
+/// <reference path='../typings/chai/chai.d.ts'/>
 
-import chai = require("chai");
-import main = require("../main");
-import ts = require("typescript");
+import chai = require('chai');
+import main = require('../main');
+import ts = require('typescript');
 
 describe('transpile to dart', function() {
 
@@ -13,43 +13,66 @@ describe('transpile to dart', function() {
 
   describe('variables', function() {
     it('should print variable declaration with initializer', function() {
-      expectTranslate("var a:number = 1;").to.equal(" num a = 1 ;\n");
+      expectTranslate('var a:number = 1;').to.equal(' num a = 1 ;\n');
     });
     it('should print variable declaration', function () {
-      expectTranslate("var a:number;").to.equal(" num a ;\n");
+      expectTranslate('var a:number;').to.equal(' num a ;\n');
     });
   });
 
   describe('classes', function() {
     it('should translate classes', function() {
-      expectTranslate("class X {}").to.equal(" class X {\n }\n");
+      expectTranslate('class X {}').to.equal(' class X {\n }\n');
     });
     it('should support extends', function() {
-      expectTranslate("class X extends Y {}").to.equal(" class X extends Y {\n }\n");
+      expectTranslate('class X extends Y {}').to.equal(' class X extends Y {\n }\n');
     });
     it('should support implements', function() {
-      expectTranslate("class X implements Y, Z {}").to.equal(" class X implements Y , Z {\n }\n");
+      expectTranslate('class X implements Y, Z {}').to.equal(' class X implements Y , Z {\n }\n');
     });
     it('should support implements', function() {
-      expectTranslate("class X extends Y implements Z {}")
-          .to.equal(" class X extends Y implements Z {\n }\n");
+      expectTranslate('class X extends Y implements Z {}')
+          .to.equal(' class X extends Y implements Z {\n }\n');
     });
 
     describe('members', function() {
       it('supports fields', function() {
-        expectTranslate("class X { x: number; }").to.equal(" class X {\n num x ; }\n");
+        expectTranslate('class X { x: number; }').to.equal(' class X {\n num x ; }\n');
       });
       it('supports field initializers', function() {
-        expectTranslate("class X { x: number = 42; }").to.equal(" class X {\n num x = 42 ; }\n");
+        expectTranslate('class X { x: number = 42; }').to.equal(' class X {\n num x = 42 ; }\n');
       });
       it('supports methods', function() {
-        expectTranslate("class X { x() { return 42; } }")
-            .to.equal(" class X {\n x ( ) { return 42 ; } }\n");
+        expectTranslate('class X { x() { return 42; } }')
+            .to.equal(' class X {\n x ( ) { return 42 ; } }\n');
       });
       it('supports method return types', function() {
-        expectTranslate("class X { x(): number { return 42; } }")
-            .to.equal(" class X {\n num x ( ) { return 42 ; } }\n");
+        expectTranslate('class X { x(): number { return 42; } }')
+            .to.equal(' class X {\n num x ( ) { return 42 ; } }\n');
       });
+      it('supports method params', function() {
+        expectTranslate('class X { x(a, b) { return 42; } }')
+            .to.equal(' class X {\n x ( a , b ) { return 42 ; } }\n');
+      });
+      it('supports method return types', function() {
+        expectTranslate('class X { x( a : number, b : string ) { return 42; } }')
+            .to.equal(' class X {\n x ( num a , String b ) { return 42 ; } }\n');
+      });
+    });
+  });
+
+  describe('functions', function() {
+    it('supports declarations', function() {
+      expectTranslate('function x() {}')
+          .to.equal(' x ( ) { }');
+    });
+    it('supports param default values', function() {
+      expectTranslate('function x(a = 42) { return 42; }')
+          .to.equal(' x ( [ a = 42 ] ) { return 42 ; }');
+    });
+    it('does not support var args', function() {
+      chai.expect(() => translateSource('function x(...a: number) { return 42; }'))
+          .to.throw('rest parameters are unsupported');
     });
   });
 });
@@ -59,23 +82,23 @@ export function translateSource(contents: string): string {
   var compilerOptions: ts.CompilerOptions = { target: ts.ScriptTarget.ES6, module: ts.ModuleKind.AMD };
   var compilerHost: ts.CompilerHost = {
     getSourceFile: function (filename, languageVersion) {
-      if (filename === "file.ts")
-        return ts.createSourceFile(filename, contents, compilerOptions.target, "0");
-      if (filename === "lib.d.ts")
-        return ts.createSourceFile(filename, '', compilerOptions.target, "0");
+      if (filename === 'file.ts')
+        return ts.createSourceFile(filename, contents, compilerOptions.target, '0');
+      if (filename === 'lib.d.ts')
+        return ts.createSourceFile(filename, '', compilerOptions.target, '0');
       return undefined;
     },
     writeFile: function (name, text, writeByteOrderMark) {
       result = text;
     },
-    getDefaultLibFilename: function () { return "lib.d.ts"; },
+    getDefaultLibFilename: function () { return 'lib.d.ts'; },
     useCaseSensitiveFileNames: function () { return false; },
     getCanonicalFileName: function (filename) { return filename; },
-    getCurrentDirectory: function () { return ""; },
-    getNewLine: function () { return "\n"; }
+    getCurrentDirectory: function () { return ''; },
+    getNewLine: function () { return '\n'; }
   };
   // Create a program from inputs
-  var program = ts.createProgram(["file.ts"], compilerOptions, compilerHost);
+  var program = ts.createProgram(['file.ts'], compilerOptions, compilerHost);
   if (program.getDiagnostics().length > 0) {
     // Throw first error.
     var first = program.getDiagnostics()[0];

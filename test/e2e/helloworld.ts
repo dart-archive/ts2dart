8e4f2a99a32dfc362b27c883890f64e406ba5aae
Martin Probst <martin@probst.io>
2015-03-17 14:43:48 -0700

    Add an end2end test using a class.

--- test/e2e/helloworld.ts
+++ test/e2e/helloworld.ts
@@ -1,5 +1,17 @@
 import t = require("unittest/unittest");
 
+class MyClass {
+  field: string;
+
+  MyClass(someVal: string) { this.field = someVal; }
+
+  getField(): string { return this.field + " world"; }
+}
+
 function main(): void {
   t.test("bigifies text", function() { t.expect("hello".toUpperCase(), t.equals("HELLO")); });
+  t.test("handles classes", function() {
+    var mc = new MyClass("hello");
+    t.expect(mc.field.toUpperCase(), t.equals("HELLO WORLD"));
+  });
 }

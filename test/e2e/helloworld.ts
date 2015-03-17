cd6409d4ef9b2ade14aebce140d8119c7f274356
Martin Probst <martin@probst.io>
2015-03-17 16:21:24 -0700

    Fix the test case. Remove superfluous test line.

--- test/e2e/helloworld.ts
+++ test/e2e/helloworld.ts
@@ -9,9 +9,8 @@ class MyClass {
 }
 
 function main(): void {
-  t.test("bigifies text", function() { t.expect("hello".toUpperCase(), t.equals("HELLO")); });
   t.test("handles classes", function() {
     var mc = new MyClass("hello");
-    t.expect(mc.field.toUpperCase(), t.equals("HELLO WORLD"));
+    t.expect(mc.getField().toUpperCase(), t.equals("HELLO WORLD"));
   });
 }

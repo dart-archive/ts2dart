93c8e5e7351e3222f41952029f60d4cebca2d7f8
Martin Probst <martin@probst.io>
2015-04-11 17:42:34 -0700

    Extract expression handling from main.

---
+++ lib/expression.ts
@@ -0,0 +1,66 @@
+/// <reference path='../node_modules/typescript/bin/typescript.d.ts' />
+import ts = require('typescript');
+import base = require('./base');
+import ts2dart = require('./main');
+
+class ExpressionTranspiler extends base.TranspilerStep {
+  constructor(tr: ts2dart.Transpiler) { super(tr); }
+
+  visitNode(node: ts.Node): boolean {
+    switch (node.kind) {
+      case ts.SyntaxKind.BinaryExpression:
+        var binExpr = <ts.BinaryExpression>node;
+        var operatorKind = binExpr.operatorToken.kind;
+        if (operatorKind === ts.SyntaxKind.EqualsEqualsEqualsToken ||
+            operatorKind === ts.SyntaxKind.ExclamationEqualsEqualsToken) {
+          if (operatorKind === ts.SyntaxKind.ExclamationEqualsEqualsToken) this.emit('!');
+          this.emit('identical (');
+          this.visit(binExpr.left);
+          this.emit(',');
+          this.visit(binExpr.right);
+          this.emit(')');
+        } else {
+          this.visit(binExpr.left);
+          if (operatorKind === ts.SyntaxKind.InstanceOfKeyword) {
+            this.emit('is');
+          } else {
+            this.emit(ts.tokenToString(binExpr.operatorToken.kind));
+          }
+          this.visit(binExpr.right);
+        }
+        break;
+      case ts.SyntaxKind.PrefixUnaryExpression:
+        var prefixUnary = <ts.PrefixUnaryExpression>node;
+        this.emit(ts.tokenToString(prefixUnary.operator));
+        this.visit(prefixUnary.operand);
+        break;
+      case ts.SyntaxKind.PostfixUnaryExpression:
+        var postfixUnary = <ts.PostfixUnaryExpression>node;
+        this.visit(postfixUnary.operand);
+        this.emit(ts.tokenToString(postfixUnary.operator));
+        break;
+      case ts.SyntaxKind.ConditionalExpression:
+        var conditional = <ts.ConditionalExpression>node;
+        this.visit(conditional.condition);
+        this.emit('?');
+        this.visit(conditional.whenTrue);
+        this.emit(':');
+        this.visit(conditional.whenFalse);
+        break;
+      case ts.SyntaxKind.DeleteExpression:
+        this.reportError(node, 'delete operator is unsupported');
+        break;
+      case ts.SyntaxKind.VoidExpression:
+        this.reportError(node, 'void operator is unsupported');
+        break;
+      case ts.SyntaxKind.TypeOfExpression:
+        this.reportError(node, 'typeof operator is unsupported');
+        break;
+      default:
+        return false;
+    }
+    return true;
+  }
+}
+
+export = ExpressionTranspiler;

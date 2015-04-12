c35b7fd4c92ead0afda5b2248598a676ec10f56a
Martin Probst <martin@probst.io>
2015-04-11 18:20:17 -0700

    Extract statements.

--- lib/main.ts
+++ lib/main.ts
@@ -14,6 +14,7 @@ import base = require('./base');
 import CallTranspiler = require('./call');
 import DeclarationTranspiler = require('./declaration');
 import ExpressionTranspiler = require('./expression');
+import StatementTranspiler = require('./statement');
 
 export interface TranspilerOptions {
   // Fail on the first error, do not collect multiple. Allows easier debugging as stack traces lead
@@ -43,7 +44,8 @@ export class Transpiler {
     this.transpilers = [
       new CallTranspiler(this),
       new DeclarationTranspiler(this),
-      new ExpressionTranspiler(this)
+      new ExpressionTranspiler(this),
+      new StatementTranspiler(this),
     ];
   }
 
@@ -382,114 +384,6 @@ export class Transpiler {
         this.emit(')');
         break;
 
-      case ts.SyntaxKind.VariableStatement:
-        var variableStmt = <ts.VariableStatement>node;
-        this.visit(variableStmt.declarationList);
-        this.emit(';');
-        break;
-      case ts.SyntaxKind.ExpressionStatement:
-        var expr = <ts.ExpressionStatement>node;
-        this.visit(expr.expression);
-        this.emit(';');
-        break;
-      case ts.SyntaxKind.SwitchStatement:
-        var switchStmt = <ts.SwitchStatement>node;
-        this.emit('switch (');
-        this.visit(switchStmt.expression);
-        this.emit(')');
-        this.visit(switchStmt.caseBlock);
-        break;
-      case ts.SyntaxKind.CaseBlock:
-        this.emit('{');
-        this.visitEach((<ts.CaseBlock>node).clauses);
-        this.emit('}');
-        break;
-      case ts.SyntaxKind.CaseClause:
-        var caseClause = <ts.CaseClause>node;
-        this.emit('case');
-        this.visit(caseClause.expression);
-        this.emit(':');
-        this.visitEach(caseClause.statements);
-        break;
-      case ts.SyntaxKind.DefaultClause:
-        this.emit('default :');
-        this.visitEach((<ts.DefaultClause>node).statements);
-        break;
-      case ts.SyntaxKind.IfStatement:
-        var ifStmt = <ts.IfStatement>node;
-        this.emit('if (');
-        this.visit(ifStmt.expression);
-        this.emit(')');
-        this.visit(ifStmt.thenStatement);
-        if (ifStmt.elseStatement) {
-          this.emit('else');
-          this.visit(ifStmt.elseStatement);
-        }
-        break;
-
-      case ts.SyntaxKind.ForStatement:
-        var forStmt = <ts.ForStatement>node;
-        this.emit('for (');
-        if (forStmt.initializer) this.visit(forStmt.initializer);
-        this.emit(';');
-        if (forStmt.condition) this.visit(forStmt.condition);
-        this.emit(';');
-        if (forStmt.iterator) this.visit(forStmt.iterator);
-        this.emit(')');
-        this.visit(forStmt.statement);
-        break;
-      case ts.SyntaxKind.ForInStatement:
-        // TODO(martinprobst): Dart's for-in loops actually have different semantics, they are more
-        // like for-of loops, iterating over collections.
-        var forInStmt = <ts.ForInStatement>node;
-        this.emit('for (');
-        if (forInStmt.initializer) this.visit(forInStmt.initializer);
-        this.emit('in');
-        this.visit(forInStmt.expression);
-        this.emit(')');
-        this.visit(forInStmt.statement);
-        break;
-      case ts.SyntaxKind.WhileStatement:
-        var whileStmt = <ts.WhileStatement>node;
-        this.emit('while (');
-        this.visit(whileStmt.expression);
-        this.emit(')');
-        this.visit(whileStmt.statement);
-        break;
-      case ts.SyntaxKind.DoStatement:
-        var doStmt = <ts.DoStatement>node;
-        this.emit('do');
-        this.visit(doStmt.statement);
-        this.emit('while (');
-        this.visit(doStmt.expression);
-        this.emit(') ;');
-        break;
-
-      case ts.SyntaxKind.TryStatement:
-        var tryStmt = <ts.TryStatement>node;
-        this.emit('try');
-        this.visit(tryStmt.tryBlock);
-        if (tryStmt.catchClause) {
-          this.visit(tryStmt.catchClause);
-        }
-        if (tryStmt.finallyBlock) {
-          this.emit('finally');
-          this.visit(tryStmt.finallyBlock);
-        }
-        break;
-      case ts.SyntaxKind.CatchClause:
-        var ctch = <ts.CatchClause>node;
-        if (ctch.variableDeclaration.type) {
-          this.emit('on');
-          this.visit(ctch.variableDeclaration.type);
-        }
-        this.emit('catch');
-        this.emit('(');
-        this.visit(ctch.variableDeclaration.name);
-        this.emit(')');
-        this.visit(ctch.block);
-        break;
-
       // Literals.
       case ts.SyntaxKind.NumericLiteral:
         var sLit = <ts.LiteralExpression>node;
@@ -633,34 +527,6 @@ export class Transpiler {
           this.emit('extends');
           this.visit(typeParam.constraint);
         }
-        break;
-
-      case ts.SyntaxKind.EmptyStatement:
-        this.emit(';');
-        break;
-      case ts.SyntaxKind.ReturnStatement:
-        var retStmt = <ts.ReturnStatement>node;
-        this.emit('return');
-        if (retStmt.expression) this.visit(retStmt.expression);
-        this.emit(';');
-        break;
-      case ts.SyntaxKind.BreakStatement:
-      case ts.SyntaxKind.ContinueStatement:
-        var breakContinue = <ts.BreakOrContinueStatement>node;
-        this.emit(breakContinue.kind == ts.SyntaxKind.BreakStatement ? 'break' : 'continue');
-        if (breakContinue.label) this.visit(breakContinue.label);
-        this.emit(';');
-        break;
-      case ts.SyntaxKind.ThrowStatement:
-        this.emit('throw');
-        this.visit((<ts.ThrowStatement>node).expression);
-        this.emit(';');
-        break;
-
-      case ts.SyntaxKind.Block:
-        this.emit('{');
-        this.visitEach((<ts.Block>node).statements);
-        this.emit('}');
         break;
 
       case ts.SyntaxKind.ImportDeclaration:

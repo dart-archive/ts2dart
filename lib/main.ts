cb9b6e68fc3e58cd8dbfb5e4490f5033b96cdf12
Vikram Subramanian <viks@google.com>
2015-11-04 15:58:16 -0800

    Make recursiveMkdirSync static

--- lib/main.ts
+++ lib/main.ts
@@ -106,7 +106,7 @@ export class Transpiler {
         .forEach((f: ts.SourceFile) => {
           var dartCode = this.translate(f);
           var outputFile = this.getOutputPath(f.fileName, destinationRoot);
-          this.recursiveMkdirSync(path.dirname(outputFile));
+          Transpiler.recursiveMkdirSync(path.dirname(outputFile));
           fs.writeFileSync(outputFile, dartCode);
         });
     this.checkForErrors(program);
@@ -173,7 +173,7 @@ export class Transpiler {
     return this.output.getResult();
   }
 
-  private recursiveMkdirSync(p: string) {
+  private static recursiveMkdirSync(p: string) {
     // Simplified version of mkdirSync from https://github.com/jprichardson/node-fs-extra
     // to avoid bringing in whole lot of module dependencies.
     var o777 = parseInt('0777', 8);
@@ -186,8 +186,8 @@ export class Transpiler {
     } catch (error) {
       switch (error.code) {
         case 'ENOENT':
-          this.recursiveMkdirSync(path.dirname(p));
-          this.recursiveMkdirSync(p);
+          Transpiler.recursiveMkdirSync(path.dirname(p));
+          Transpiler.recursiveMkdirSync(p);
           break;
 
         // In the case of any other error, just see if there's a dir

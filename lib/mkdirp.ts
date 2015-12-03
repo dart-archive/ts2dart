9ef9a234ea0bae5d570b204dd4195c12e27eafce
Vikram Subramanian <viks@viks-macbookpro.roam.corp.google.com>
2015-12-02 22:19:44 -0800

    Fix mkdirp implementation to take relative path names and call fs.mkdirSync instead of fs.mkdir

--- lib/mkdirp.ts
+++ lib/mkdirp.ts
@@ -4,11 +4,14 @@ import * as path from 'path';
 
 export default function mkdirP(p: string) {
   let absPath = process.cwd();
+  if (path.isAbsolute(p)) {
+    p = path.relative(absPath, p);
+  }
 
   p.split(path.sep).forEach(dirName => {
     absPath = path.join(absPath, dirName);
     if (!fs.existsSync(absPath)) {
-      fs.mkdir(absPath);
+      fs.mkdirSync(absPath);
     }
   })
 }
